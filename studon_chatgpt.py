7ace43cefe9809b9d62e95b3b2c42cfb02c9f1ee
coalbl4ck <yannik.lorenz.1999@gmail.com>
2023-02-19 21:19:19 +0100

    studon_chatgpt.py | Improved question forwarding & Changed Temperature & BugFixes
    Manual Testing of the heat parameter indicates that the temperature=1
    yields the best results.
    The main problem right now is the format and parsing of the questions.
    One can already judge the quality of the result by the parsed question.
    
    The prediction rate for the training exam is 66%, leaving one question
    that could not be answered because it requires an image and another
    that is text generation and was not graded.
    
    Closes #1

--- studon_chatgpt.py
+++ studon_chatgpt.py
@@ -12,17 +12,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import ElementNotVisibleException, TimeoutException
+from selenium.common.exceptions import ElementNotVisibleException, NoSuchElementException, TimeoutException
 
 # CONSTANTS
 URL_LOGIN = "https://www.studon-exam.fau.de/winter22/login.php?lang=de"
+QUESTION_OUTER_CLASS = 'ilc_question_Standard'
 
 # Variables
 input_queue = queue.Queue()
 
 def add_input(input_queue):
     while True:
-        # input_queue.put(sys.stdin.read(1))
         input_queue.put(input())
 
 def parse_credentials():
@@ -43,24 +43,69 @@ def ask_chatgpt(question, context=''):
     print(question)
     model_engine = "text-davinci-003"
 
+    prompt = "context:" + context + "\n\n" + "prompt:" + question
+    prompt = prompt.strip()
+
     completion = openai.Completion.create(
         engine=model_engine,
-        prompt="context:" + context + "\n\n" + "prompt:" + question,
-        max_tokens=1024,
+        prompt=prompt,
+        max_tokens=2048,
         n=1,
-        stop=None,
-        temperature=0.7)
+        temperature=1,
+        stream=True)
     # Model Response
-    response = completion.choices[0].text
     print("=========")
     print("ChatGPT: ", flush=True)
     print("=========")
-    print(response)
-    print("\n Ask another Question?", end=' ', flush=True)
+    response = ''
+    for resp in completion:
+        streamed = resp.choices[0].text
+        print(streamed, end="", flush=True)
+        response += streamed
+
+    if len(response) == 0:
+        print("\n\n Tell ChatGPT what to do:", end=' ', flush=True)
+    else:
+        print("\n\n Ask ChatGPT:", end=' ', flush=True)
     context += "\n".join([context, question, response])
     return (context, response)
 
 
+def build_chatgpt_question(question_standard):
+    child_elements = question_standard.find_elements(By.XPATH, './/*')
+
+    question_type = child_elements[0].get_dom_attribute("class")
+    question_text = ''
+    try:
+        question_text = '%s\n' % question_standard.find_element(By.CLASS_NAME, 'ilc_qtitle_Title').text
+    except NoSuchElementException:
+        pass
+
+    match question_type:
+        case 'ilc_question_MultipleChoice':
+            for answer in question_standard.find_elements(By.CLASS_NAME, 'answertext'):
+                question_text += '* %s\n' % answer.text
+        case 'ilc_question_KprimChoice':
+            question_text += '%s\n' % question_standard.find_element(By.CLASS_NAME, 'ilAssKprimInstruction').text
+            for answer in question_standard.find_elements(By.CLASS_NAME, 'ilc_qanswer_Answer')[1 :]:
+                question_text += '* %s\n' % answer.text
+        case 'ilc_question_OrderingQuestion':
+            for item in question_standard.find_elements(By.CLASS_NAME, 'ilc_qordli_OrderListItem'):
+                question_text += '* %s\n' % item.text
+        case 'ilc_question_ClozeTest':
+            question_text = question_standard.text
+            question_text += '\n%s' % 'Fill the blanks!'
+        case 'ilc_question_MatchingQuestion':
+            question_text += "%s\n" % question_standard.find_element(By.ID, 'targetArea').text
+            question_text += "%s\n" % question_standard.find_element(By.ID, 'sourceArea').text
+        case 'ilc_question_FormulaQuestion':
+            question_text += "%s\n" % question_standard.find_element(By.CLASS_NAME, 'ilc_question_FormulaQuestion').text
+        case 'ilc_question_TextQuestion':
+            pass
+        case _:
+            print(">>> Warning: Question type not recognized", file=sys.stderr)
+            question_text = question_standard.text
+    return question_text
 
 if __name__ == '__main__':
     username, password, openai_api_key = parse_credentials()
@@ -84,9 +129,6 @@ if __name__ == '__main__':
 
     driver.find_element(By.NAME, "cmd[doStandardAuthentication]").click()
 
-
-    title_class = 'ilc_qtitle_Title'
-
     current_page = driver.current_url
     question_answered = False
 
@@ -95,7 +137,7 @@ if __name__ == '__main__':
 
     while True:
         if question_element is not None:
-            wait_stale = WebDriverWait(driver, timeout=0)
+            wait_stale = WebDriverWait(driver, timeout=1)
             try:
                 wait_stale.until(EC.staleness_of(question_element))
                 # Question has become stale
@@ -105,23 +147,24 @@ if __name__ == '__main__':
                 os.system('clear')
             except TimeoutException:
                 if not input_queue.empty():
-                    input = input_queue.get()
-                    ask_chatgpt(input, context)
+                    user_input = input_queue.get()
+                    context, _ = ask_chatgpt(user_input, context)
                 else:
                     time.sleep(1)
         else:
             wait_question = WebDriverWait(driver, timeout=100, poll_frequency=1
                                           , ignored_exceptions=[ElementNotVisibleException])
             try:
-                wait_question.until(EC.presence_of_element_located((By.CLASS_NAME, title_class)))
-                question_element = driver.find_element(By.CLASS_NAME, title_class)
-                question = question_element.text
+                wait_question.until(EC.presence_of_element_located((By.CLASS_NAME, 'ilc_question_Standard')))
+
+                question_element = driver.find_element(By.CLASS_NAME, QUESTION_OUTER_CLASS)
+
+                question = build_chatgpt_question(question_element)
                 # Ask chatgpt
                 context, _ = ask_chatgpt(question)
-            except:
-                pass
-            finally:
-                time.sleep(3)
+
+            except TimeoutException:
+                continue
 
 
 
